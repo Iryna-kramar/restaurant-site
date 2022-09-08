c9c5dbc7fc7b830b2b4c3a181e67790eabb31721
Iryna Kramar <irynakramar@Irynas-MacBook-Air.local>
2022-09-08 14:31:04 +0300

    sections added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
     <nav className="app__navbar">
-      <div className="app__app__navbar-logo">
+      <div className="app__navbar-logo">
         <img src={images.gericht} alt="app logo" />
       </div>
       <ul className="app__navbar-links">
